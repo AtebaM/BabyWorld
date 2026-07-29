1eaf556f9c1cc08c5f600912ef8911f284574959
Benoit <benoitmbole4@gmail.com>
2026-07-29 09:27:32 +0200

    Models created

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,9 @@
 # from django.db import models
 
 # Create your models here.
+class Tags:
+    def __init__(self, name,id,created_at,updated_at):
+        self.name = name
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
